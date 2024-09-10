6015da073b7503f4900f6ee4508231a2023659b9
Alizhan <alizhan20052@gmail.com>
2024-09-10 16:07:44 +0500

    first change

---
+++ app/src/main/java/eu/example/myrecipeapp/RecipeAPp.kt
@@ -0,0 +1,37 @@
+package eu.example.myrecipeapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import eu.tutorials.myrecipeapp.Category
+import eu.tutorials.myrecipeapp.MainViewModel
+import eu.tutorials.myrecipeapp.RecipeScreen
+
+
+@Composable
+fun RecipeApp(navController: NavHostController){
+
+    val recipeViewModel: MainViewModel = viewModel()
+    val viewState by recipeViewModel.categoriesState
+
+  
+    NavHost(navController = navController, startDestination = Screen.RecipeScreen.route){
+        composable(route = Screen.RecipeScreen.route){
+            RecipeScreen(viewState = viewState, navigateToDetail = {
+                navController.currentBackStackEntry?.savedStateHandle?.set("cat" , it)
+                navController.navigate(Screen.DetailScreen.route)
+
+            })
+        }
+        composable(route = Screen.DetailScreen.route){
+            val category = navController.previousBackStackEntry?.savedStateHandle?.
+            get<Category>("cat") ?: Category("", "", "", "")
+            CategoryDetailScreen(category = category)
+        }
+    }
+   
+
+}
---
+++ app/src/main/java/eu/example/myrecipeapp/Screen.kt
@@ -0,0 +1,8 @@
+package eu.example.myrecipeapp
+
+sealed class Screen(val route: String) {
+
+    object RecipeScreen:Screen("recipescreen")
+    object DetailScreen:Screen("detailscreen")
+
+}
